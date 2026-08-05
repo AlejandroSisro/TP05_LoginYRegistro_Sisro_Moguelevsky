6a0bea46d4b954c1f2eb548341b20c50a711344b
50233806-create <50233806@est.ort.edu.ar>
2026-08-05 09:06:00 -0300

    Add Usuario class with user properties

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,1 +1,15 @@
+namespace TPs.Models;
+public class Usuario
+{
+public string NombreUsuario{get; set;}
+public string Contraseña {get;set;}
+public string Nombre {get;set;}
+public string Apellido {get; set;}
+public string TipoDeUsuario{get;set;}
+
+public Usuario()
+{
+    
+}
+}
 
