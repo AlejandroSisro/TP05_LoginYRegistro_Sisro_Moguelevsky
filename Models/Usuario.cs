975faa480365b129cbb32fa624663554fef30e72
ALEJANDRO SISRO <50434059@est.ort.edu.ar>
2026-08-18 09:05:18 -0300

    Add idCiudad property to Usuario model

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -6,6 +6,7 @@ public string contraseña {get;set;}
 public string nombre {get;set;}
 public string apellido {get; set;}
 public string tipoUsuario{get;set;}
+public int idCiudad {get; set;}
 
 public Usuario()
 {
